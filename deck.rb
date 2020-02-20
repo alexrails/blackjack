82d3e3c325f10fb2f064fb204c40ca31849388e6
Alex <alexrails@yandex.ru>
2020-02-20 23:04:33 +0300

    Create Deck and Player classes

---
+++ deck.rb
@@ -0,0 +1,16 @@
+class Deck
+  attr_accessor :amount, :card_rang, :card_suit, :current_deck
+  def initialize(amount)
+    @amount = amount.to_i
+    @card_rang = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
+    @card_suit = [ "♢", "♣", "♡", "♠" ]
+    @current_deck = []
+    @card_rang.each do |rang|
+      amount.times do
+        @card_suit.each do |suit|
+          @current_deck << [rang, suit]
+        end
+      end
+    end
+  end
+end
---
+++ player.rb
@@ -0,0 +1,13 @@
+class Player
+  attr_accessor :name, :hand, :bank
+
+  def initialize(name)
+    @name = name
+    @bank = 100
+    @hand = []
+  end
+
+  def get_cards(number)
+
+  end
+end
